6ad768d54d522e4dcd9810c9ccedb81e7eab29ec
Alex <maslenok.f1@gmail.com>
2014-02-25 17:20:35 +0400

    edit class

--- src/main/java/pack/HelloClass.java
+++ src/main/java/pack/HelloClass.java
@@ -3,7 +3,7 @@ package pack;
 public class HelloClass {
     public static void main(String[] args) {
 
-        System.out.println("Hello!"); // Display the string.
+        System.out.println("Hello and GoodBye!"); // Display the string.
 
     }
 }
