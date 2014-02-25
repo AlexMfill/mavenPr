3611b0e25a1d2c025a89a884a56cec1ff5b5b596
AlexMfill <maslenok.f@gmail.com>
2014-02-25 05:21:46 -0800

    Update HelloClass.java

--- src/main/java/pack/HelloClass.java
+++ src/main/java/pack/HelloClass.java
@@ -3,7 +3,7 @@ package pack;
 public class HelloClass {
     public static void main(String[] args) {
 
-        System.out.println("Hello and GoodBye!"); // Display the string.
+        System.out.println("Hello WORLD!"); // Display the string.
 
     }
 }
