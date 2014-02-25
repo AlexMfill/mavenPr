e344b6108fd5bc7be90285420620d421d62c8abf
Alex <maslenok.f1@gmail.com>
2014-02-25 17:15:37 +0400

    add

---
+++ src/main/java/pack/HelloClass.java
@@ -0,0 +1,12 @@
+package pack;
+
+/**
+ * Created by Alex on 25/02/14.
+ */
+public class HelloClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!"); // Display the string.
+
+    }
+}
